a89e4debf0cfe211093958433bb2e6a3343599de
Steven Enriquez <stevenenriquez123@gmail.com>
2023-04-09 10:44:00 -0700

    schema.prisma vercel webpack plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
+
 // @ts-check
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
@@ -16,6 +18,13 @@ const config = {
     },
     images: {
         domains: ['cdn.discordapp.com']
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()]
+        }
+
+        return config
     }
 };
 export default config;
