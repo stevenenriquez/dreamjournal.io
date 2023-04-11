3b56b4715cc31cb766db73146d372df9c97c0c40
Steven Enriquez <stevenenriquez123@gmail.com>
2023-04-11 08:59:34 -0700

    styling

--- src/app/dream/[slug]/page.tsx
+++ src/app/dream/[slug]/page.tsx
@@ -20,14 +20,22 @@ export default async function Dream(context: { params: Record<string, string> })
     const dream = await getDream();
 
     if(dream) {
+
+        let wordCount = dream.content.split(' ').length;
+        let readTime = Math.round(wordCount / 130);
+
         return (
-            <div className='text-center m-4'>
+            <div className='border border-4 border-gray-800 rounded-lg min-h-[80vh] p-8 m-6 mt-4 md:mx-16 lg:mx-32 xl:mx-64 text-center'>
                 <h1>{dream.title}</h1>
                 <div className='mt-6 text-purple-300'>
                     <Image src={dream.author.image || '/favicon.ico'} alt={dream.author?.name || 'User Avatar'} width={64} height={64} className='rounded-full w-8 h-8 inline-block mr-2' />
                     {dream.author.name}
                 </div>
-                <p className='m-16 xl:mx-64 xl:mt-8 mt-8 text-gray-400 text-center whitespace-pre-wrap'>{dream.content}</p>
+                {readTime >= 1 && (
+                    <div className="text-gray-500 m-4">{readTime} minute read</div>
+                )}
+                <div className="border border-b-1 border-gray-800 mt-6" />
+                <p className='text-start mt-8 text-gray-400 text-center whitespace-pre-wrap'>{dream.content}</p>
             </div>
         )
     } else {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
     const dreams = await getDreams();
 
     return (
-        <div className='flex min-h-screen flex-wrap justify-center m-6'>
+        <div className='flex min-h-screen flex-wrap justify-center mx-1 mb-2 mt-6 sm:m-6'>
             {dreams.map((dream: Dream & {author: User}) => <DreamEntry dream={dream} key={dream.id} />)}
         </div>
     );
