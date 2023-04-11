4fbdd652f4c820789e1162c6053f129355a49f06
Steven Enriquez <stevenenriquez123@gmail.com>
2023-04-10 20:05:54 -0700

    render newlines for dream content

--- src/app/dream/[slug]/page.tsx
+++ src/app/dream/[slug]/page.tsx
@@ -23,11 +23,11 @@ export default async function Dream(context: { params: Record<string, string> })
         return (
             <div className='text-center m-4'>
                 <h1>{dream.title}</h1>
-                <p className='mt-4 text-gray-400'>{dream.content}</p>
                 <div className='mt-6 text-purple-300'>
                     <Image src={dream.author.image || '/favicon.ico'} alt={dream.author?.name || 'User Avatar'} width={64} height={64} className='rounded-full w-8 h-8 inline-block mr-2' />
                     {dream.author.name}
                 </div>
+                <p className='m-16 xl:mx-64 xl:mt-8 mt-8 text-gray-400 text-center whitespace-pre-wrap'>{dream.content}</p>
             </div>
         )
     } else {
