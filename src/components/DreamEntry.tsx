bc5f59f9165320e843cf7832e29284ea3daf6d9e
Steven Enriquez <stevenenriquez123@gmail.com>
2023-04-10 19:20:16 -0700

    lg: 2 cols, else 1 on homepage

--- src/components/DreamEntry.tsx
+++ src/components/DreamEntry.tsx
@@ -6,8 +6,8 @@ import path from '../../src/utils/paths';
 
 export default function DreamEntry(props: { dream: Dream & {author: User} }) {
     return (
-        <Link href={`${path.dream}/[slug]`} as={`${path.dream}/${props.dream.id}`} className='border-2 border-gray-800 rounded-lg m-6 p-4 w-11/12 hover:bg-gray-900 transition ease-in-out hover:scale-105'>
-            <h1 className='mt-2'>{truncate(props.dream.title, 20)}</h1>
+        <Link href={`${path.dream}/[slug]`} as={`${path.dream}/${props.dream.id}`} className='border-2 border-gray-800 rounded-lg p-4 w-11/12 m-6 lg:w-5/12 lg:m-3 hover:bg-gray-900 transition ease-in-out hover:scale-105'>
+            <h1 className='mt-2'>{truncate(props.dream.title)}</h1>
             <p className='mt-2 text-gray-400'>{truncate(props.dream.content)}</p>
             <div className='mt-2 w-max text-purple-300'>
                 <Image src={props.dream.author.image || '/favicon.ico'} alt={props.dream.author.name || 'User Avatar'} width={64} height={64} className='rounded-full w-8 h-8 inline-block mr-2' />
