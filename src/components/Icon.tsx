d03e631f3d0003cff032cb9ee9cd27e6caabe82a
Steven Enriquez <stevenenriquez123@gmail.com>
2023-04-14 00:41:24 -0700

    add back btn

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -62,4 +62,8 @@ export const Home = (
 
 export const AddDream = (
     <path strokeLinecap="round" strokeLinejoin="round" d="M.5 13.5h11m-5-3.5l-3 .54L4 7.5L10.73.79a1 1 0 0 1 1.42 0l1.06 1.06a1 1 0 0 1 0 1.42Z"/>
-)
+);
+
+export const Back = (
+    <path d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42a.996.996 0 0 0-1.41 0l-6.59 6.59a.996.996 0 0 0 0 1.41l6.59 6.59a.996.996 0 1 0 1.41-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1z"/>
+);
