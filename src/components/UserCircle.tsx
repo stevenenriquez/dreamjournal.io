754e1b1cd21a9cf7a5eaa3fbff3d1901aa5c4012
Steven Enriquez <stevenenriquez123@gmail.com>
2023-04-14 00:38:05 -0700

    css for mobile UserIcon

--- src/components/UserCircle.tsx
+++ src/components/UserCircle.tsx
@@ -28,11 +28,11 @@ export default function UserCircle() {
                     alt="User"
                     width={64}
                     height={64}
-                    className="float-right h-14 w-14 rounded-full border border-black hover:border-gray-600 hover:scale-110 transition ease-in-out"
+                    className="float-right h-10 w-10 rounded-full border border-black hover:border-gray-600 hover:scale-110 transition ease-in-out"
                 />
             </button>
             {userMenuToggle ? (
-                <ul className="absolute left-6 w-24 p-2 text-right transition ease-in-out bg-gray-200 hover:bg-gray-300 rounded-lg">
+                <ul className="absolute sm:static right-6 bottom-14 w-24 p-2 text-right transition ease-in-out bg-gray-200 hover:bg-gray-300 rounded-lg">
                     <li className="text-center">
                         <button onClick={() => logOut()}>Logout</button>
                     </li>
